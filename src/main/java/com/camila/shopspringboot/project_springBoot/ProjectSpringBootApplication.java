3fcae3707bed3513a873de2fb0c8d2a0f793d738
Camila Cerqueira <camila_cerqueira@sicredi.com.br>
2022-08-15 18:03:17 -0300

    init

---
+++ src/main/java/com/camila/shopspringboot/project_springBoot/ProjectSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.camila.shopspringboot.project_springBoot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectSpringBootApplication.class, args);
+	}
+
+}
