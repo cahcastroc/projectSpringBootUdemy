231ff81d8643ad500580009c00a9060478d228c8
Camila Cerqueira <camila_cerqueira@sicredi.com.br>
2022-08-15 20:27:37 -0300

    343- Teste com h2

--- src/main/java/com/camila/shopspringboot/project_springBoot/entities/User.java
+++ src/main/java/com/camila/shopspringboot/project_springBoot/entities/User.java
@@ -1,12 +1,19 @@
 package com.camila.shopspringboot.project_springBoot.entities;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 import java.io.Serializable;
 import java.util.Objects;
 
+@Entity
 public class User implements Serializable {
 
-  private static final long serialVersionUID=1L; //Nº DE SERIÉ DO SERIABLIZABLE
+    private static final long serialVersionUID=1L; //Nº DE SERIÉ DO SERIABLIZABLE
 
+    @Id
+    @GeneratedValue (strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
     private String email;
