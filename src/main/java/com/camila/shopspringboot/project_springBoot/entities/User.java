efc63c0eaeb4ef59fc38e5499f8da2908fb97c2b
Camila Cerqueira <camila_cerqueira@sicredi.com.br>
2022-08-16 13:42:50 -0300

    346 - Entity Order

--- src/main/java/com/camila/shopspringboot/project_springBoot/entities/User.java
+++ src/main/java/com/camila/shopspringboot/project_springBoot/entities/User.java
@@ -1,13 +1,13 @@
 package com.camila.shopspringboot.project_springBoot.entities;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Objects;
 
 @Entity
+@Table(name="tb_user")
 public class User implements Serializable {
 
     private static final long serialVersionUID=1L; //Nº DE SERIÉ DO SERIABLIZABLE
@@ -19,6 +19,8 @@ public class User implements Serializable {
     private String email;
     private String phone;
     private String password;
+    @OneToMany(mappedBy = "client")
+    private List<Order> orders = new ArrayList<>();
 
     public User() {
     }
@@ -29,6 +31,10 @@ public class User implements Serializable {
         this.email = email;
         this.phone = phone;
         this.password = password;
+    }
+
+    public List<Order> getOrders() {
+        return orders;
     }
 
     public Long getId() {
